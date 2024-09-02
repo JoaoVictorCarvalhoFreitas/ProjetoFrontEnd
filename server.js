074caab61294ba80ac4e54c74d95aa2ec320830e
FZhuu <felipezhangzhu@gmail.com>
2024-09-02 18:49:06 -0300

    changes
    pacote express instalado, mudança no output do server.js e inicio da pagina de login

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.static('paginas'));
 
 app.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+  console.log(`Servidor ouvindo na porta http://localhost:3000`);
 });
