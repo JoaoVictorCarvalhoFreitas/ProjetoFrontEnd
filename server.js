b5252e13a1459a518610bd040eee7cd1d24925e2
Joao Victor Carvalho <jvcarvalho018@gmail.com>
2024-09-02 16:29:03 -0300

    tirando alguns modulos que n precisava

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 import express from 'express'
-import http from 'http'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { setupSocketIO } from './socketHandler.mjs';
 
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
-const server = http.createServer(app);
 
-// Configura o Socket.IO
-const io = setupSocketIO(server);
+app.use(express.static('paginas'));
 
-app.use(express.static(path.join(__dirname, 'paginas')));
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
 });
