792fded2bedfe9e0a252d6d95079b5f11a5d1c68
FZhuu <felipezhangzhu@gmail.com>
2024-09-03 16:26:43 -0300

    Signup side att

--- paginas/scripts/login.js
+++ paginas/scripts/login.js
@@ -5,19 +5,32 @@ function cadastro() {
 function mostrarCadastro() {
     var img = document.getElementById("image");
     var card = document.getElementById("card");
-    img.style.borderRadius = "1vi 0 0 1vi"
-    img.style.transition = "ease-in-out 0.5s"
-    
+
+
     var login = document.getElementById("login-side");
     var cadastro = document.getElementById("cadastro-side");
-    login.style.transition = "ease-in-out 0.5s"
-    cadastro.style.transition = "ease-in-out 0.5s"
-    login.style.opacity = 0;
-    setTimeout(HideLogin,600);
-    cadastro.style.opacity = 0;
-    cadastro.style.display = "flex";
-    setTimeout(ShowSignup, 600);
-    
+
+    if (login.style.display == "none") {
+        cadastro.style.opacity = 0;
+        setTimeout(HideSignup,600);
+        img.style.borderRadius = "0 1vi 1vi 0"
+        img.style.transition = "ease-in-out 0.5s"
+        img.style.transform = "translateX(100%)"
+        setTimeout(row, 600);
+        login.style.opacity = 1;
+        setTimeout(ShowLogin, 600);
+    } else {
+        login.style.opacity = 0;
+        setTimeout(HideLogin,600);
+        img.style.borderRadius = "1vi 0 0 1vi"
+        img.style.transition = "ease-in-out 0.5s"
+        img.style.transform = "translateX(-100%)"
+        setTimeout(rowreverse, 600);
+        cadastro.style.opacity = 0;
+        setTimeout(ShowSignup, 600);
+    }
+
+
 }
 
 function HideLogin() {
@@ -25,22 +38,65 @@ function HideLogin() {
     login.style.display = "none";
 }
 
+function ShowLogin() {
+    var login = document.getElementById("login-side");
+    login.style.opacity = 1;
+    login.style.display = "flex";
+}
+
+function HideSignup() {
+    var cadastro = document.getElementById("cadastro-side");
+    cadastro.style.display = "none";
+}
 
 function ShowSignup() {
     var cadastro = document.getElementById("cadastro-side");
     cadastro.style.opacity = 1;
+    cadastro.style.display = "flex";
 }
 
-var checkbox = document.getElementById("mostrarSenha");
+function rowreverse() {
+    var img = document.getElementById("image");
+    var card = document.getElementById("card");
+    img.style.transition = "none"
+    card.style.flexDirection = "row-reverse";
+    img.style.transform = "translateX(0%)"
+}
+function row() {
+    var img = document.getElementById("image");
+    var card = document.getElementById("card");
+    img.style.transition = "none"
+    card.style.flexDirection = "row";
+    img.style.transform = "translateX(0%)"
+
+}
+
+
+
+var checkbox1 = document.getElementById("mostrarSenha");
 
 checkbox.addEventListener("click", function() {
     var senha = document.getElementById("senha-login");
     var img = document.getElementById("eye");
-    if (checkbox.checked) {
+    if (checkbox1.checked) {
         senha.type = "text";
         img.src = "src/eye-off-svgrepo-com.svg";
     } else {
         senha.type = "password";
         img.src = "src/eye-svgrepo-com.svg";
+    }
+});
+
+var checkbox2 = document.getElementById("checkbox");
+
+checkbox.addEventListener("click", function() {
+    var senha = document.getElementById("senha-cadastro");
+    var confirmsenha = document.getElementById("confirmar-senha");
+    if (checkbox2.checked) {
+        senha.type = "text";
+        confirmsenha.type = "text";
+    } else {
+        senha.type = "password";
+        confirmsenha.type = "password";
     }
 });
