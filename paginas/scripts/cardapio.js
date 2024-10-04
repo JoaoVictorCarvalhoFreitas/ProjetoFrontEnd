c21fa80e9a9cd2c07b01fd6ba0b3b4e5fab1e528
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-10-03 21:25:16 -0300

    Card Produto

--- paginas/scripts/cardapio.js
+++ paginas/scripts/cardapio.js
@@ -6,10 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 <div class="card card-menu" id="prodDiv2">
                     <img src="${prod.imagemUrl || 'https://via.placeholder.com/300'}" class="card-img-top" alt="${prod.nome}">
                     <div class="card-body">
-                        <h5 class="card-title" id="prodNome">${prod.nome}</h5>
+                        <h5 class="card-title produtoText" id="prodNome">${prod.nome}</h5>
+                        <p class=produtoText>Descrição</p>
                         <p class="card-text" id="prodPreco">R$ ${parseFloat(prod.preco).toFixed(2)}</p>
-                        <a class="btn btn-primary" id="comprar" href="#">Comprar</a>
-                        <input id="prodQuantidade" type="number" class="form-control" value="1" min="1" max="10">
+                        <div class="precoComprar">
+                            <input id="prodQuantidade" type="number" class="form-control" value="1" min="1" style="margin-right: 5%;">
+                            <a class="btn btn-primary" id="comprar" href="#"><h5>Comprar</h5></a>
+                        </div>
                     </div>
                 </div>
             </div>
