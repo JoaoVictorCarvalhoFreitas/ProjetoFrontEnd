642b85a7159b16bfd1e4ebd44ab106e47b265a04
Joao Victor Carvalho <jvcarvalho018@gmail.com>
2024-10-24 21:04:10 -0300

    .

--- paginas/scripts/cardapio.js
+++ paginas/scripts/cardapio.js
@@ -48,6 +48,7 @@ function adEventList(){
 
     function criarCardProduto(prod) {
         return `
+        <div class="col-md-4 mb-4" id="prodDiv1">
             <div class="card card-menu" id="prodDiv2" >
                 <img src="${prod.imagemUrl || 'https://via.placeholder.com/300'}" class="card-img-top" alt="${prod.nome}">
                 <div class="card-body">
@@ -67,6 +68,7 @@ function adEventList(){
                     </div>
                 </div>
             </div>
+        </div>
         `;
     }
 
@@ -75,7 +77,7 @@ function adEventList(){
             const resp = await fetch('/produtos');
             const produtos = await resp.json();
             
-            const menuContainer = document.getElementById('prodDiv1');
+            const menuContainer = document.getElementById('menuContainer');
             menuContainer.innerHTML = produtos.map(criarCardProduto).join('');
             // Agora que os produtos foram carregados, adicionar os event listeners
             adEventList();
